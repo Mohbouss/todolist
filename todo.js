1b1d82c51c5d03393359cd022e16c3c778dcf2dd
Mohbouss <mohamedamine.boussaida@etudiant-isi.utm.tn>
2023-06-17 13:45:05 +0200

    todolist v2

--- todo.js
+++ todo.js
@@ -1,35 +1,93 @@
-let input = document.getElementById('my-input')
+let input1 = document.getElementById('my-input1');
+let input2 = document.getElementById('my-input2')
 let TaskListContainer = document.getElementById('task-container');
-console.log(TaskListContainer)
-let AddButton =document.getElementById('add-btn').addEventListener('click',addtask)
-
-
-function addtask(){
-let taskDescription = input.value;
-if(taskDescription !==''){
-let taskElement = CreateMyElement('li')
-let taskContent = CreateMyElement('span',taskDescription,'task-box')
-let deleteButton= CreateMyElement('button','Delete','delete-button')
-deleteButton.addEventListener('click',deleteTask);
-taskContent.addEventListener('click',taskDone)
-taskElement.appendChild(taskContent)
-taskElement.appendChild(deleteButton)
-TaskListContainer.appendChild(taskElement);
-input.value = '';
-
-}}
- function CreateMyElement(tagName, textContent, className){
-    var element = document.createElement(tagName);
+let UserListContainer = document.getElementById('user-container');
+document.getElementById('add-btn').addEventListener('click',addtask)
+document.getElementById('add-user').addEventListener('click',AddUser);
+document.getElementById('returnToUsers').addEventListener('click', returnToUsers);
+let arrayoftasks=[]
+let arrayofUsers=[]
+function CreateMyElement(tagName, textContent, className){
+    let element = document.createElement(tagName);
     element.textContent = textContent;
     element.classList.add(className);
     return element;
  }
+
+function showtask(array){
+    TaskListContainer.innerHTML = '';
+    for(let i=0; i<array.length;i++){
+    let taskElement = CreateMyElement('li')
+    let taskContent = CreateMyElement('span',array[i],'task-box')
+    let deleteButton= CreateMyElement('button','Delete','delete-button')
+    deleteButton.addEventListener('click',deleteTask);
+    taskContent.addEventListener('click',taskDone)
+    taskElement.appendChild(taskContent)
+    taskElement.appendChild(deleteButton)
+    TaskListContainer.appendChild(taskElement);    
+}}
+function showUsers(array){
+    UserListContainer.innerHTML = '';
+    for(let i=0; i<array.length;i++){
+    let UserElement = CreateMyElement('li')
+    let UserContent = CreateMyElement('span',array[i],'Users-box')
+    let deleteButton= CreateMyElement('button','Delete','delete-button')
+    deleteButton.addEventListener('click',deleteUser);
+    //UserContent.addEventListener('click',taskDone)
+    UserElement.appendChild(UserContent)
+    UserElement.appendChild(deleteButton)
+    UserListContainer.appendChild(UserElement);   
+    UserContent.addEventListener('click', toggleDivs); 
+}}
+
+//code for page1
+function AddUser(){
+    let UserName = input1.value;
+    if(UserName !==''){
+    arrayofUsers.push(UserName)
+    showUsers(arrayofUsers)
+    input1.value = '';
+    }}
+
+    function deleteUser(event){
+        arrayofUsers.pop()
+        event.target.parentNode.remove()
+    }
+  
+      
+ 
+
+//code for page2
+
+function addtask(){
+let taskDescription = input2.value;
+
+if(taskDescription !==''){
+arrayoftasks.push(taskDescription)
+showtask(arrayoftasks)
+input2.value = '';
+}}
 function deleteTask(event){
+    arrayoftasks.pop()
     event.target.parentNode.remove()
 }
 function taskDone(event){
     event.target.classList.toggle('active')
 }
 
+function toggleDivs() {
+    let page1 = document.querySelector('#page1');
+    let page2 = document.querySelector('#page2');
+    page1.classList.add('hidden');
+    page2.classList.remove('hidden');
+  }
+  
+  function returnToUsers() {
+    let page1 = document.querySelector('#page1');
+    let page2 = document.querySelector('#page2');
+    page1.classList.remove('hidden');
+    page2.classList.add('hidden');
+  }
 
 
+ 
