fa7124923be8862b8e6f07ea13d646d04119326c
Mohbouss <mohamedamine.boussaida@etudiant-isi.utm.tn>
2023-06-23 11:55:35 +0200

    fixed style

--- todo.js
+++ todo.js
@@ -1,122 +1,7 @@
-/*   let AddUserInput = document.getElementById('add-user-input');
-  let AddTaskInput = document.getElementById('add-task-input');
-  let TaskListContainer = document.getElementById('task-container');
-  let UserListContainer = document.getElementById('user-container');
-  document.getElementById('add-task-btn').addEventListener('click', addTask);
-  document.getElementById('add-user-btn').addEventListener('click', addUser);
-  document.getElementById('return-to-users').addEventListener('click', returnToUsers);
-  
-  let users = [];
-  let activeUser = null;
-  
-  function CreateMyElement(tagName, textContent = "", className ="") {
-    let element = document.createElement(tagName);
-    element.textContent = textContent;
-    className? element.classList.add(className):null;
-    return element;
-  }
-  
-  function showTask() {
-    TaskListContainer.innerHTML = '';
-  
-    if (activeUser) {
-      for (let i = 0; i < activeUser.tasks.length; i++) {
-        let taskElement = CreateMyElement('li');
-        let taskContent = CreateMyElement('span', activeUser.tasks[i].task, 'task-box');
-        let deleteButton = CreateMyElement('button', 'Delete', 'delete-button');
-        if(activeUser.tasks[i].completed){
-          taskContent.classList.add('active')
-        }
-        
-        deleteButton.addEventListener('click', () => deleteTask(i));
-        taskContent.addEventListener('click', () => taskDone(i));
-  
-        taskElement.appendChild(taskContent);
-        taskElement.appendChild(deleteButton);
-        TaskListContainer.appendChild(taskElement);
-      }
-    }
-    
-  }
-  
-  function addTask() {
-    let taskDescription = AddTaskInput.value.trim();
-  
-    if (taskDescription !== '' && activeUser) {
-      activeUser.tasks.push({task:taskDescription,completed:false});
-      showTask();
-      AddTaskInput.value = '';
-    }
-  }
-  
-  function deleteTask(index) {
-    activeUser.tasks.splice(index, 1);
-    showTask();
-  }
-  
-  function showUsers() {
-    UserListContainer.innerHTML = '';
-  
-    for (let i = 0; i < users.length; i++) {
-      let userElement = CreateMyElement('li');
-      let userContent = CreateMyElement('span', users[i].name, 'users-box');
-      let deleteButton = CreateMyElement('button', 'Delete', 'delete-button');
-  
-      deleteButton.addEventListener('click', () => deleteUser(i));
-      userContent.addEventListener('click', () => toggleDivs(i));
-  
-      userElement.appendChild(userContent);
-      userElement.appendChild(deleteButton);
-      UserListContainer.appendChild(userElement);
-    }
-  }
-  
-  function addUser() {
-    let userName = AddUserInput.value.trim();
-  
-    if (userName !== '') {
-      let newUser = { name: userName, tasks: [] };
-      users.push(newUser);
-      showUsers();
-      AddUserInput.value = '';
-    }
-  }
-  
-  function deleteUser(index) {
-    users.splice(index, 1);
-    showUsers();
-    if (index === activeUser) {
-      activeUser = null;
-      showTask();
-    }
-  }
-  
-  function taskDone(index) {
-    //taskElement.classList.toggle('active');
-    activeUser.tasks[index].completed = !activeUser.tasks[index].completed
-    showTask();
-  }
-  
-  function toggleDivs(index) {
-    activeUser = users[index];
-    let page1 = document.querySelector('#page1');
-    let page2 = document.querySelector('#page2');
-    page1.classList.add('hidden');
-    page2.classList.remove('hidden');
-    showTask();
-  }
-  
-  function returnToUsers() {
-    activeUser = null;
-    let page1 = document.querySelector('#page1');
-    let page2 = document.querySelector('#page2');
-    page1.classList.remove('hidden');
-    page2.classList.add('hidden');
-    showUsers();
-  }
-   */
 let AddUserInput = document.getElementById('add-user-input');
 let AddTaskInput = document.getElementById('add-task-input');
+let header = document.querySelector('#page2 header');
+
 let TaskListContainer = document.getElementById('task-container');
 let UserListContainer = document.getElementById('user-container');
 document.getElementById('add-task-btn').addEventListener('click', addTask);
@@ -268,11 +153,7 @@ function deleteUser(userId) {
     console.error(error);
   });
 }
-/* function taskDone(index) {
-  //taskElement.classList.toggle('active');
-  activeUser.
-  showTask();
-} */
+
 function taskDone(taskId) {
   fetch(`http://localhost:8000/tasks/${taskId}/state`)
     .then(response => response.json())
@@ -312,13 +193,14 @@ function toggleDivs(userId) {
       let page2 = document.querySelector('#page2');
       page1.classList.add('hidden');
       page2.classList.remove('hidden');
+      header.textContent = activeUser[0].name
+
       showTask();
     })
     .catch(error => {
       console.error(error);
     });
 }
-
 function returnToUsers() {
   activeUser = null;
   let page1 = document.querySelector('#page1');
